4686e3ee8d2fdf59196170cfc23f0481ef43cc5a
SaiPavanD <saipvn4@gmail.com>
2016-11-09 00:25:18 +0530

    neural network

---
+++ assgn2/MLFFNN/prob_1c.m
@@ -0,0 +1,67 @@
+root = '../data/overlapping_data/';
+numClasses = 4;
+numHiddenLayers = 2;
+step = 0.05;
+
+inputs = [];
+targets = [];
+
+for i = 1:numClasses
+   temp_path = 'class';
+   temp_path = strcat(root,temp_path,int2str(i),'_train.txt');
+   temp_data = dlmread(temp_path)';
+   inputs = [inputs temp_data];
+   temp_targets = zeros(numClasses,size(temp_data,2));
+   temp_targets(i,:) = temp_targets(i,:) + 1;
+   targets = [targets temp_targets];
+end
+
+[~,trainInd] = size(inputs);
+[~,trainClassfn] = max(targets);
+
+for i = 1:numClasses
+   temp_path = 'class';
+   temp_path = strcat(root,temp_path,int2str(i),'_val.txt');
+   temp_data = dlmread(temp_path)';
+   inputs = [inputs temp_data];
+   temp_targets = zeros(numClasses,size(temp_data,2));
+   temp_targets(i,:) = temp_targets(i,:) + 1;
+   targets = [targets temp_targets];
+end
+
+[~,valInd] = size(inputs);
+
+for i = 1:numClasses
+   temp_path = 'class';
+   temp_path = strcat(root,temp_path,int2str(i),'_test.txt');
+   temp_data = dlmread(temp_path)';
+   inputs = [inputs temp_data];
+   temp_targets = zeros(numClasses,size(temp_data,2));
+   temp_targets(i,:) = temp_targets(i,:) + 1;
+   targets = [targets temp_targets];
+end
+
+[~,testInd] = size(inputs);
+
+net = patternnet(numHiddenLayers);
+net.trainParam.goal=1e-6;
+net.divideFcn = 'divideind';
+net.divideParam.trainInd = 1:trainInd;
+net.divideParam.valInd = trainInd+1:valInd;
+net.divideParam.testInd = valInd+1:testInd;
+[net,tr] = train(net,inputs,targets);
+
+[x,y] = meshgrid(min(inputs(1,:)):step:max(inputs(1,:)),min(inputs(2,:)):step:max(inputs(2,:)));
+decisionRegionPoints = [x(:)';y(:)'];
+decisionRegionOutputs = net(decisionRegionPoints);
+[~,decisionRegionClassfn] = max(decisionRegionOutputs);
+
+colors=[1 0.75 0.75; 0.75 1 0.75; 0.75 0.75 1; 1 1 0.75];
+plot1 = gscatter(decisionRegionPoints(1,:),decisionRegionPoints(2,:),decisionRegionClassfn,colors,'****',[],'off');
+hold on;
+xlabel('Dimension 1');
+ylabel('Dimension 2');
+plot2 = gscatter(inputs(1,1:trainInd),inputs(2,1:trainInd),trainClassfn,'rgby','....');
+hold on;
+legend([plot2],'Class 1','Class 2','Class 3','Class 4');
+title('Overlapping data using MLFFNN');
