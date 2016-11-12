2007179864ebd4604f59aa70cb499ee9934c56b2
SaiPavanD <saipvn4@gmail.com>
2016-11-12 13:03:02 +0530

    Linear c-svm

--- assgn2/MLFFNN/prob_2.m
+++ assgn2/MLFFNN/prob_2.m
@@ -38,18 +38,3 @@ net.divideParam.trainRatio = 0.75;
 net.divideParam.valRatio = 0.15;
 net.divideParam.testRatio = 0.15;
 [net,tr] = train(net,inputs,targets);
-
-% [x,y] = meshgrid(min(inputs(1,:)):step:max(inputs(1,:)),min(inputs(2,:)):step:max(inputs(2,:)));
-% decisionRegionPoints = [x(:)';y(:)'];
-% decisionRegionOutputs = net(decisionRegionPoints);
-% [~,decisionRegionClassfn] = max(decisionRegionOutputs);
-% 
-% colors=[1 0.75 0.75; 0.75 1 0.75; 0.75 0.75 1; 1 1 0.75];
-% plot1 = gscatter(decisionRegionPoints(1,:),decisionRegionPoints(2,:),decisionRegionClassfn,colors,'****',[],'off');
-% hold on;
-% xlabel('Dimension 1');
-% ylabel('Dimension 2');
-% plot2 = gscatter(inputs(1,1:trainInd),inputs(2,1:trainInd),trainClassfn,'rgby','....');
-% hold on;
-% legend([plot2],'Class 1','Class 2','Class 3','Class 4');
-% title('Overlapping data using MLFFNN');
--- assgn2/SVM/p3.m
+++ assgn2/SVM/p3.m
@@ -7,6 +7,8 @@ train_data = [];
 train_labels = [];
 test_data = [];
 test_labels = [];
+val_data = [];
+val_labels = [];
 
 for i = 1:numClasses
    temp_path = 'class';
@@ -18,15 +20,15 @@ for i = 1:numClasses
    train_labels = [train_labels ; temp_labels];
 end
 
-% for i = 1:numClasses
-%    temp_path = 'class';
-%    temp_path = strcat(root,temp_path,int2str(i),'_val.txt');
-%    temp_data = dlmread(temp_path);
-%    train_data = [train_data ; temp_data];
-%    temp_labels = zeros(size(temp_data,1),1);
-%    temp_labels(:,1) = i;
-%    train_labels = [train_labels temp_labels];
-% end
+for i = 1:numClasses
+   temp_path = 'class';
+   temp_path = strcat(root,temp_path,int2str(i),'_val.txt');
+   temp_data = dlmread(temp_path);
+   val_data = [val_data ; temp_data];
+   temp_labels = zeros(size(temp_data,1),1);
+   temp_labels(:,1) = i;
+   val_labels = [val_labels ; temp_labels];
+end
 
 for i = 1:numClasses
    temp_path = 'class';
@@ -39,9 +41,10 @@ for i = 1:numClasses
 end
 
 model = svmtrain(train_labels, train_data,'-s 0 -t 0 -q -c 2');
-% output_labels = svmpredict(test_labels, test_data, model);
+output_labels_val = svmpredict(val_labels, val_data, model);
+output_labels_test = svmpredict(test_labels, test_data, model);
 
-[x,y] = meshgrid(-20:step:20,-20:step:20);
+[x,y] = meshgrid(-17:step:17,-17:step:20);
 decisionRegionPoints = [x(:),y(:)];
 dummyLabels = zeros(size(decisionRegionPoints,1),1);
 decisionRegionOutputs = svmpredict(dummyLabels, decisionRegionPoints, model,'-q');
@@ -53,5 +56,6 @@ xlabel('Dimension 1');
 ylabel('Dimension 2');
 plot2 = gscatter(train_data(:,1),train_data(:,2),train_labels,'rgby','....');
 hold on;
+axis([-22 +22 -22 +22]);
 legend([plot2],'Class 1','Class 2','Class 3','Class 4');
-title('Linearly seperable data using MLFFNN');
+title('Linearly seperable data using C-SVM');
